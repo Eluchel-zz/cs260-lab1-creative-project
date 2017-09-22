01176d344b8c9857f8a01f7ebce44158831dd630
Ben Hall <aydustr@gmail.com>
2017-09-22 00:51:14 -0600

    final changes

--- js/customScripts.js
+++ js/customScripts.js
@@ -1,8 +1,10 @@
 $(function() {
-    console.log("test");
     $("#generate").click(function(){
         var environment = $("#environmentSelect").find(":selected").text();
         environment = environment.toLowerCase();
         $("body").css("background-image", "url('images/" + environment + "_background1')");
+
+        var randomNum = Math.floor(Math.random() * 10 + 3);
+        $("#result").text(randomNum + " Orcs attacked");
     });
 });
