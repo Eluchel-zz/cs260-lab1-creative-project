9346e0cc33db21ea34a8abe8b8ecee15a02acc90
Ben Hall <aydustr@gmail.com>
2017-09-22 00:19:03 -0600

    added js fixes

--- js/customScripts.js
+++ js/customScripts.js
@@ -1,7 +1,8 @@
 $(function() {
+    console.log("test");
     $("#generate").click(function(){
         var environment = $("#environmentSelect").find(":selected").text();
         environment = environment.toLowerCase();
         $("body").css("background-image", "url('images/" + environment + "_background1')");
     });
-}
+});
