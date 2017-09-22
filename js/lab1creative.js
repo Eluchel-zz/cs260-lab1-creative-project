99440c5fb23424c852271e03266423f3eb2fee52
Ben Hall <aydustr@gmail.com>
2017-09-21 23:16:34 -0600

    Add files via upload

--- js/lab1creative.js
+++ js/lab1creative.js
@@ -1,4 +1,8 @@
-function switchBackground(event) {
-  var body = document.getElementsTagName("BODY")[0];
-  body.style.backgroundImage = "url('images/" + event.target.value + "_background1.jpg')";
+function switchBackground() {
+  /*var body = document.getElementsTagName("BODY")[0];
+  body.style.backgroundImage = "url('images/" + event.target.value + "_background1.jpg')";*/
+
+  var environment = $("#environmentSelect").find(":selected").text();
+  environment = environment.toLowerCase();
+  $("body").css("background-image", "url('images/" + environment + "_background1')");
 }
